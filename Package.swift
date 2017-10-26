0036fe9cf2a41d816b05162f1187546553d49129
Ricardo Olivieri <roliv@us.ibm.com>
2017-10-26 17:08:08 -0500

    Moved all codable closure aliases to contracts.

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura-net.git", .upToNextMinor(from: "1.7.0")),
         .package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", .upToNextMinor(from: "1.7.0")),
-        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", .upToNextMinor(from: "0.0.12"))
+        .package(url: "https://github.com/IBM-Swift/KituraContracts.git", .upToNextMinor(from: "0.0.13"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
